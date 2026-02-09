1356542e55ae71b6461c0e3c06f9712dac07894c
m-karakus <s.metinkarakus@gmail.com>
2026-02-09 14:20:50 +0300

    new structure

--- mkpipe_loader_snowflake/__init__.py
+++ mkpipe_loader_snowflake/__init__.py
@@ -1,21 +1,22 @@
-from mkpipe.functions_spark import BaseLoader
+from mkpipe.spark import JdbcLoader
 
 
-class SnowflakeLoader(BaseLoader):
-    def __init__(self, config, settings):
-        super().__init__(
-            config,
-            settings,
-            driver_name='snowflake',
-            driver_jdbc='net.snowflake.client.jdbc.SnowflakeDriver',
-        )
+class SnowflakeLoader(JdbcLoader, variant='snowflake'):
+    driver_name = 'snowflake'
+    driver_jdbc = 'net.snowflake.client.jdbc.SnowflakeDriver'
 
     def build_jdbc_url(self):
-        base = f'jdbc:{self.driver_name}://{self.host}:{self.port}/?user={self.username}&warehouse={self.warehouse}&db={self.database}&schema={self.schema}'
+        base = (
+            f'jdbc:{self.driver_name}://{self.host}:{self.port}/'
+            f'?user={self.username}'
+            f'&warehouse={self.warehouse}'
+            f'&db={self.database}'
+            f'&schema={self.schema}'
+        )
         if self.private_key_file:
+            base += f'&private_key_file={self.private_key_file}'
             if self.private_key_file_pwd:
-                return f'{base}&private_key_file={self.private_key_file}&private_key_file_pwd={self.private_key_file_pwd}'
-            else:
-                return f'{base}&private_key_file={self.private_key_file}'
+                base += f'&private_key_file_pwd={self.private_key_file_pwd}'
         else:
-            return f'{base}&password={self.password}'
+            base += f'&password={self.password}'
+        return base
