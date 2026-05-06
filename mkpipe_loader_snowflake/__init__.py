6010291b65ccf1e4d48a82668415e3914bfc852d
m-karakus <s.metinkarakus@gmail.com>
2026-05-06 23:09:59 +0300

    feature: if_existes parameters added

--- mkpipe_loader_snowflake/__init__.py
+++ mkpipe_loader_snowflake/__init__.py
@@ -150,7 +150,8 @@ class SnowflakeLoader(BaseLoader, variant='snowflake'):
                 case WriteStrategy.APPEND:
                     self._write_df(df, 'append', target_name)
                 case WriteStrategy.REPLACE:
-                    self._write_df(df, 'overwrite', target_name)
+                    mode = 'append' if self.if_exists == 'append' else 'overwrite'
+                    self._write_df(df, mode, target_name)
                 case WriteStrategy.UPSERT | WriteStrategy.MERGE:
                     if not table.write_key:
                         raise ConfigError(
